430df040da44daa1729d136839a73e9588114b57
Tehnikumi Õpilane <opilane@tptlive.ee>
2016-10-19 14:24:21 +0300

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created by opilane on 19.10.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello!\nHow are you doing?\n");
+          System.out.println("Hello, World!");
+            String teade="hei";
+               System.out.println(teade); // hei ekraanile
+             int    minute= 100; //veidi rohkem minuteid
+        System.out.print(teade+minute);
+    }
+}
